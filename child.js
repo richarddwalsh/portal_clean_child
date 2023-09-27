762adab9917ada5b8c7ebd08f8b63a37592e6efb
c-rwalsh <143125972+c-rwalsh@users.noreply.github.com>
2023-09-26 23:37:52 -0600

    Fix Sub Menus, Add Resources, Fix Tabs

--- child.js
+++ child.js
@@ -1,3 +1,69 @@
+const buttons = document.querySelectorAll('[data-reach-menu-button]');
+const actionBtns = document.querySelectorAll('.minor-btn');
+const submenus = document.querySelectorAll('[data-reach-menu]');
+const triggerBtns = document.querySelectorAll('.modal-trigger');
+const cancelBtns = document.querySelectorAll('.cancel-btn');
+const modals = document.querySelectorAll('.modal');
+
+const paramMapping = {
+  'menu-button--menu--group_type': {
+    param: 'group_type',
+    valueText: 'Group Type'
+  },
+  'menu-button--menu--location_type': {
+    param: 'location_type_preference',
+    valueText: 'Location Type'
+  },
+  'menu-button--menu--target-audience': {
+    param: 'target_audience',
+    valueText: 'Target Audience'
+  },
+  'menu-button--menu--frequency': {
+    param: 'frequency',
+    valueText: 'Frequency'
+  },
+  'menu-button--menu--neighborhood': {
+    param: 'neighborhood___area',
+    valueText: 'Neighborhood | Area'
+  },
+  'menu-button--menu--timezone': {
+    param: 'time_zone',
+    valueText: 'Time Zone'
+  },
+  'menu-button--menu--day_of_week': {
+    param: 'day_of_week',
+    valueText: 'Day of Week'
+  },
+  'menu-button--menu--time_of_day': {
+    param: 'time_of_day',
+    valueText: 'Time of Day'
+  },
+  'menu-button--menu--campus': {
+    param: 'campus',
+    valueText: 'All Campuses'
+  },
+  'menu-button--menu--category': {
+    param: 'category',
+    valueText: 'All Categories'
+  },
+  'menu-button--menu--schedule': {
+    param: 'schedule',
+    valueText: 'Schedule'
+  },
+  'menu-button--menu--location': {
+    param: 'location',
+    valueText: 'Location'
+  },
+  'menu-button--menu--experience': {
+    param: 'experience',
+    valueText: 'Requires Experience'
+  },
+  'menu-button--menu--background': {
+    param: 'background',
+    valueText: 'Background'
+  },
+}
+
 // Function to set the window height as a CSS variable
 function setWindowHeightVariable() {
   const windowHeight = window.innerHeight;
@@ -15,12 +81,6 @@ window.addEventListener('load', setWindowHeightVariable);
 // Update the variable whenever the window is resized
 window.addEventListener('resize', setWindowHeightVariable);
 
-const triggerBtns = document.querySelectorAll('.modal-trigger');
-const cancelBtns = document.querySelectorAll('.cancel-btn');
-const modals = document.querySelectorAll('.modal');
-const actionBtns = document.querySelectorAll('.minor-btn');
-const submenus = document.querySelectorAll('[data-reach-menu]');
-
 function openModal(event) {
   event.stopPropagation();
   const targetModalId = event.currentTarget.getAttribute('data-target');
@@ -28,8 +88,9 @@ function openModal(event) {
   targetModal.style.display = 'block';
 }
 
-function closeModal(event) {
-  modals.forEach((modal) => {
+function closeModal() {
+  modals.forEach((el) => {
+    const modal = el;
     modal.style.display = 'none';
   });
 }
@@ -77,12 +138,13 @@ function triggerAction(event) {
       console.log(response.error);
       return
     } 
-    
-    if (returnUrl && response.status && response.status === 'success') {
-      window.location.href = returnUrl;
-    }
 
-    if (response.status && response.status === 'success') {
+    if (response.status === 'success') {
+      if (returnUrl) {
+        window.location.href = returnUrl;
+        return;
+      }
+
       window.location.reload();
     }
   })
@@ -92,23 +154,27 @@ function triggerAction(event) {
   
 }
 
-function hideAllSubmenus() {
+// Function to hide all submenus
+function hideAllSubmenus(event) {
+  console.log(event);
   submenus.forEach((submenu) => {
-    submenu.style.display = 'none';
+    const subMenuElement = submenu;
+    subMenuElement.style.display = 'none';
   });
-};
+}
 
-document.addEventListener('click', (event) => {
-  // Check if the click event is not on any button or submenu
-  const isButtonClick = Array.from(actionBtns).some((button) => button.contains(event.target));
-  const isSubmenuClick = Array.from(submenus).some((submenu) => submenu.contains(event.target));
+// document.addEventListener('click', (event) => {
+//   // Check if the click event is not on any button or submenu
+//   const isButtonClick = Array.from(buttons).some((button) => button.contains(event.target));
+//   const isActionButtonClick = Array.from(actionBtns).some((button) => button.contains(event.target));
+//   const isSubmenuClick = Array.from(submenus).some((submenu) => submenu.contains(event.target));
 
-  if (!isButtonClick && !isSubmenuClick) {
-    hideAllSubmenus();
-  }
-});
+//   if (!isButtonClick && !isSubmenuClick || !isActionButtonClick && !isSubmenuClick) {
+//     hideAllSubmenus(event);
+//   }
+// });
 
-function showSubMenu(event) {
+function showActionMenu(event) {
   event.stopPropagation();
 
   hideAllSubmenus();
@@ -126,8 +192,120 @@ function showSubMenu(event) {
   subMenu.parentNode.style.display = 'block';
 }
 
+function updateButtonsFromUrlParams() {
+  const url = new URL(window.location);
+  const {searchParams} = url;
+
+  Object.keys(paramMapping).forEach((buttonId) => {
+    const urlParamObj = paramMapping[buttonId];
+    const button = document.getElementById(buttonId);
+
+    if (!button) return;
+
+    const paramValue = searchParams.get(urlParamObj.param);
+    
+    if (paramValue === "true") { 
+      button.innerText = "Yes";
+    } else if (paramValue === "false") { 
+      button.innerText = "No";
+    } else if (paramValue) {
+      button.innerText = paramValue;
+    } else {
+      button.innerText = urlParamObj.valueText; // Reset to default text
+    }
+  });
+}
+
+// Function to update button text and URL
+function updateButtonAndUrl(event) {
+  const valueText = event.target.getAttribute('data-valuetext');
+  const buttonId = event.target.parentNode.getAttribute('aria-labelledby');
+  const button = document.getElementById(buttonId);
+
+  if (button) {
+    if (valueText === 'true') {
+      button.innerText = "Yes";
+    } else if (valueText === 'false') {
+      button.innerText = "No";
+    } else {
+      button.innerText = valueText;
+    }
+  }
+
+  // Find the corresponding URL parameter
+  const urlParamObj = paramMapping[buttonId];
+  if (!urlParamObj) {
+    console.error(`No URL parameter mapping found for button ID ${buttonId}`);
+    return;
+  }
+
+  // Update the URL
+  const url = new URL(window.location);
+  if (valueText === '(any)') {
+    button.innerText = urlParamObj.valueText;  // Reset to default label
+    url.searchParams.delete(urlParamObj.param);  // Delete the URL param
+  } else {
+    url.searchParams.set(urlParamObj.param, valueText);  // Set the URL param
+  }
+
+  // To change the URL without reloading the page
+  // window.history.pushState({}, '', url);
+
+  // Uncomment the next line if you want to reload the page after changing the URL
+  window.location.href = url;
+
+  hideAllSubmenus();
+}
+
+// Check if the number of buttons and submenus match
+if (buttons.length === submenus.length) {
+  buttons.forEach((button, index) => {
+    // Your existing code to add click event listeners and manage submenu displays
+
+    // Add click event listener to menu items within each submenu
+    const menuItems = submenus[index].querySelectorAll('[role="menuitem"]');
+    menuItems.forEach((menuItem) => {
+      menuItem.addEventListener('click', updateButtonAndUrl);
+    });
+  });
+
+  // Loop through each button to add click event listeners
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      // Hide all submenus first
+      hideAllSubmenus();
+
+      const buttonId = button.id;
+      const subMenuId = `${buttonId.split('--')[1]}--${buttonId.split('--')[2]}`;
+      const subMenu = document.getElementById(subMenuId);
+      const subMenuWrapper = subMenu.parentNode;
+      
+      const buttonRect = button.getBoundingClientRect();
+      console.log(buttonRect);
+
+      subMenuWrapper.style.left = `${buttonRect.left}px`;
+      subMenuWrapper.style.top = `${buttonRect.bottom + 15}px`;
+      subMenuWrapper.style.display = 'block';
+      console.log(subMenuWrapper);
+    });
+  });
+
+  // Add a click event listener to the document to close the submenu when clicking outside
+  document.addEventListener('click', (event) => {
+    // Check if the click event is not on any button or submenu
+    const isButtonClick = Array.from(buttons).some((button) => button.contains(event.target));
+    const isSubmenuClick = Array.from(submenus).some((submenu) => submenu.contains(event.target));
+
+    if (!isButtonClick && !isSubmenuClick) {
+      hideAllSubmenus();
+    }
+  });
+} else {
+  console.error('Number of buttons and submenus do not match.');
+}
+
 actionBtns.forEach((button) => {
-  button.addEventListener('click', showSubMenu);
+  button.addEventListener('click', showActionMenu);
   submenus.forEach((submenu) => {
     const menuItems = submenu.querySelectorAll('[role="menuitem"]');
     menuItems.forEach((menuItem) => {
@@ -151,6 +329,9 @@ function handleFormSubmit(event) {
   // Collect form data
   const form = event.currentTarget;
   const formData = new FormData(form);
+
+  const progressCircle = form.querySelector('.progress-circle');
+  progressCircle.style.display = 'block';
 
   // Convert FormData to JSON
   const formDataJSON = {};
@@ -189,4 +370,8 @@ function handleFormSubmit(event) {
 const modalForms = document.querySelectorAll('.modal form');
 modalForms.forEach((form) => {
   form.addEventListener('submit', handleFormSubmit);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  updateButtonsFromUrlParams();
 });
--- js/volunteer-list.js
+++ js/volunteer-list.js
@@ -1,146 +1,146 @@
-// Get all the buttons and submenus
-const buttons = document.querySelectorAll('[data-reach-menu-button]');
-const submenus = document.querySelectorAll('[data-reach-menu]');
+// // Get all the buttons and submenus
+// const buttons = document.querySelectorAll('[data-reach-menu-button]');
+// let submenus = document.querySelectorAll('[data-reach-menu]');
 
-// Mapping of button IDs to URL parameters
-const paramMapping = {
-  'menu-button--menu--8': {
-    param: 'schedule',
-    valueText: 'Schedule'
-  },
-  'menu-button--menu--9': {
-    param: 'location',
-    valueText: 'Location'
-  },
-  'menu-button--menu--10': {
-    param: 'experience',
-    valueText: 'Requires Experience'
-  },
-  'menu-button--menu--11': {
-    param: 'background',
-    valueText: 'Background'
-  },
-}
+// // Mapping of button IDs to URL parameters
+// const paramMapping = {
+//   'menu-button--menu--8': {
+//     param: 'schedule',
+//     valueText: 'Schedule'
+//   },
+//   'menu-button--menu--9': {
+//     param: 'location',
+//     valueText: 'Location'
+//   },
+//   'menu-button--menu--10': {
+//     param: 'experience',
+//     valueText: 'Requires Experience'
+//   },
+//   'menu-button--menu--11': {
+//     param: 'background',
+//     valueText: 'Background'
+//   },
+// }
 
-// Function to hide all submenus
-function hideAllSubmenus() {
-  submenus.forEach((submenu) => {
-    const subMenuElement = submenu;
-    subMenuElement.style.display = 'none';
-  });
-}
+// // Function to hide all submenus
+// function hideAllSubmenus() {
+//   submenus.forEach((submenu) => {
+//     const subMenuElement = submenu;
+//     subMenuElement.style.display = 'none';
+//   });
+// }
 
-function updateButtonsFromUrlParams() {
-  const url = new URL(window.location);
-  const {searchParams} = url;
+// function updateButtonsFromUrlParams() {
+//   const url = new URL(window.location);
+//   const {searchParams} = url;
 
-  Object.keys(paramMapping).forEach((buttonId) => {
-    const urlParamObj = paramMapping[buttonId];
-    const button = document.getElementById(buttonId);
+//   Object.keys(paramMapping).forEach((buttonId) => {
+//     const urlParamObj = paramMapping[buttonId];
+//     const button = document.getElementById(buttonId);
 
-    if (!button) return;
+//     if (!button) return;
 
-    const paramValue = searchParams.get(urlParamObj.param);
+//     const paramValue = searchParams.get(urlParamObj.param);
     
-    if (paramValue === "true") { 
-      button.innerText = "Yes";
-    } else if (paramValue === "false") { 
-      button.innerText = "No";
-    } else if (paramValue) {
-      button.innerText = paramValue;
-    } else {
-      button.innerText = urlParamObj.valueText; // Reset to default text
-    }
-  });
-}
+//     if (paramValue === "true") { 
+//       button.innerText = "Yes";
+//     } else if (paramValue === "false") { 
+//       button.innerText = "No";
+//     } else if (paramValue) {
+//       button.innerText = paramValue;
+//     } else {
+//       button.innerText = urlParamObj.valueText; // Reset to default text
+//     }
+//   });
+// }
 
-// Function to update button text and URL
-function updateButtonAndUrl(event) {
-  const valueText = event.target.getAttribute('data-valuetext');
-  const buttonId = event.target.parentNode.getAttribute('aria-labelledby');
-  const button = document.getElementById(buttonId);
+// // Function to update button text and URL
+// function updateButtonAndUrl(event) {
+//   const valueText = event.target.getAttribute('data-valuetext');
+//   const buttonId = event.target.parentNode.getAttribute('aria-labelledby');
+//   const button = document.getElementById(buttonId);
 
-  if (button) {
-    if (valueText === 'true') {
-      button.innerText = "Yes";
-    } else if (valueText === 'false') {
-      button.innerText = "No";
-    } else {
-      button.innerText = valueText;
-    }
-  }
+//   if (button) {
+//     if (valueText === 'true') {
+//       button.innerText = "Yes";
+//     } else if (valueText === 'false') {
+//       button.innerText = "No";
+//     } else {
+//       button.innerText = valueText;
+//     }
+//   }
 
-  // Find the corresponding URL parameter
-  const urlParamObj = paramMapping[buttonId];
-  if (!urlParamObj) {
-    console.error(`No URL parameter mapping found for button ID ${buttonId}`);
-    return;
-  }
+//   // Find the corresponding URL parameter
+//   const urlParamObj = paramMapping[buttonId];
+//   if (!urlParamObj) {
+//     console.error(`No URL parameter mapping found for button ID ${buttonId}`);
+//     return;
+//   }
 
-  // Update the URL
-  const url = new URL(window.location);
-  if (valueText === '(any)') {
-    button.innerText = urlParamObj.valueText;  // Reset to default label
-    url.searchParams.delete(urlParamObj.param);  // Delete the URL param
-  } else {
-    url.searchParams.set(urlParamObj.param, valueText);  // Set the URL param
-  }
+//   // Update the URL
+//   const url = new URL(window.location);
+//   if (valueText === '(any)') {
+//     button.innerText = urlParamObj.valueText;  // Reset to default label
+//     url.searchParams.delete(urlParamObj.param);  // Delete the URL param
+//   } else {
+//     url.searchParams.set(urlParamObj.param, valueText);  // Set the URL param
+//   }
 
-  // To change the URL without reloading the page
-  // window.history.pushState({}, '', url);
+//   // To change the URL without reloading the page
+//   // window.history.pushState({}, '', url);
 
-  // Uncomment the next line if you want to reload the page after changing the URL
-  window.location.href = url;
+//   // Uncomment the next line if you want to reload the page after changing the URL
+//   window.location.href = url;
 
-  hideAllSubmenus();
-}
+//   hideAllSubmenus();
+// }
 
-// Check if the number of buttons and submenus match
-if (buttons.length === submenus.length) {
-  buttons.forEach((button, index) => {
-    // Your existing code to add click event listeners and manage submenu displays
+// // Check if the number of buttons and submenus match
+// if (buttons.length === submenus.length) {
+//   buttons.forEach((button, index) => {
+//     // Your existing code to add click event listeners and manage submenu displays
 
-    // Add click event listener to menu items within each submenu
-    const menuItems = submenus[index].querySelectorAll('[role="menuitem"]');
-    menuItems.forEach((menuItem) => {
-      menuItem.addEventListener('click', updateButtonAndUrl);
-    });
-  });
+//     // Add click event listener to menu items within each submenu
+//     const menuItems = submenus[index].querySelectorAll('[role="menuitem"]');
+//     menuItems.forEach((menuItem) => {
+//       menuItem.addEventListener('click', updateButtonAndUrl);
+//     });
+//   });
 
-  // Loop through each button to add click event listeners
-  buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      // Hide all submenus first
-      hideAllSubmenus();
+//   // Loop through each button to add click event listeners
+//   buttons.forEach((button, index) => {
+//     button.addEventListener('click', () => {
+//       // Hide all submenus first
+//       hideAllSubmenus();
 
-      // Show the submenu associated with the clicked button
-      submenus[index].style.display = 'block';
+//       // Show the submenu associated with the clicked button
+//       submenus[index].style.display = 'block';
 
-      // Calculate the position to place the submenu below the button
-      const buttonRect = button.getBoundingClientRect();
-      const buttonHeight = buttonRect.height;
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+//       // Calculate the position to place the submenu below the button
+//       const buttonRect = button.getBoundingClientRect();
+//       const buttonHeight = buttonRect.height;
+//       const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-      // Set the position of the submenu
-      submenus[index].style.left = `${buttonRect.left}px`;
-      submenus[index].style.top = `${buttonRect.top + buttonHeight + scrollTop}px`;
-    });
-  });
+//       // Set the position of the submenu
+//       submenus[index].style.left = `${buttonRect.left}px`;
+//       submenus[index].style.top = `${buttonRect.top + buttonHeight + scrollTop}px`;
+//     });
+//   });
 
-  // Add a click event listener to the document to close the submenu when clicking outside
-  document.addEventListener('click', (event) => {
-    // Check if the click event is not on any button or submenu
-    const isButtonClick = Array.from(buttons).some((button) => button.contains(event.target));
-    const isSubmenuClick = Array.from(submenus).some((submenu) => submenu.contains(event.target));
+//   // Add a click event listener to the document to close the submenu when clicking outside
+//   document.addEventListener('click', (event) => {
+//     // Check if the click event is not on any button or submenu
+//     const isButtonClick = Array.from(buttons).some((button) => button.contains(event.target));
+//     const isSubmenuClick = Array.from(submenus).some((submenu) => submenu.contains(event.target));
 
-    if (!isButtonClick && !isSubmenuClick) {
-      hideAllSubmenus();
-    }
-  });
-} else {
-  console.error('Number of buttons and submenus do not match.');
-}
+//     if (!isButtonClick && !isSubmenuClick) {
+//       hideAllSubmenus();
+//     }
+//   });
+// } else {
+//   console.error('Number of buttons and submenus do not match.');
+// }
 
-window.addEventListener('DOMContentLoaded', () => {
-  updateButtonsFromUrlParams();
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   updateButtonsFromUrlParams();
+// });
